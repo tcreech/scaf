d48dbcb2d0937db7377d004960cd17ee8e404a6b
Tim Creech <tcreech@umd.edu>
2013-07-20 21:24:46 -0400

    add missing early kill (linux)
    Note this one. Performance and/or measurements may be altered by this
    change.

--- src/libscaf.c
+++ src/libscaf.c
@@ -714,6 +714,7 @@ void* scaf_gomp_training_control(void *unused){
 #endif //__sun
       usleep(100000);
 #if defined(__linux__)
+      kill(expPid, SIGALRM);
       waitpid(expPid, &status, 0);
 #endif //__linux__
 #if defined(__sun)
