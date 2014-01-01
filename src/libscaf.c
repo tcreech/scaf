40e44594b555d2d15205e37d7ed5fd6d6736a287
Tim Creech <tcreech@umd.edu>
2014-01-01 11:13:13 -0500

    adding the missing kill again. I think it is safe.

--- src/libscaf.c
+++ src/libscaf.c
@@ -724,6 +724,7 @@ static void* scaf_gomp_training_control(void *unused){
 #endif //__sun
       usleep(100000);
 #if defined(__linux__)
+      kill(expPid, SIGALRM);
       waitpid(expPid, &status, 0);
 #endif //__linux__
 #if defined(__sun)
