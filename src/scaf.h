c4f9f4a6a04cad0cad948c345ba78a91659f047b
Tim Creech <tcreech@umd.edu>
2014-10-22 14:56:15 -0400

    clean up scaf.h

--- src/scaf.h
+++ src/scaf.h
@@ -2,7 +2,6 @@
 #define SCAF_H
 
 #define SCAF_CONNECT_STRING "ipc:///tmp/ipc-scafd"
-//#define SCAF_CONNECT_STRING "tcp://localhost:5555"
 
 // Set default hash function to  Paul Hsieh's. Seems to do lookups slightly
 // faster on our keys.
